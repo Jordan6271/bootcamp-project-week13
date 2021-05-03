57cd5448efc3d8b334d736cf11bcc4af36bb2367
Jordan6271 <jordan.robertson-browne@mattressonline.co.uk>
2021-05-03 16:06:29 +0100

    Allow functions to go off other params

--- controller.js
+++ controller.js
@@ -16,7 +16,10 @@ exports.create = function (request, response, next) {
 	}
 	if (typeof request.body.read !== `boolean`) {
 		return next(
-			createError(400, `Read status of book must be a boolean value`)
+			createError(
+				400,
+				`Read status of book must exist and be a boolean value`
+			)
 		);
 	}
 	booklist.push({
@@ -30,24 +33,92 @@ exports.create = function (request, response, next) {
 };
 
 exports.show = function (request, response, next) {
-	const booklistitem = booklist.find((book) => book.id == request.params.id);
-	if (!booklistitem) {
-		return next(createError(404, `There is no book with that id`));
+	const booklistId = booklist.find((book) => book.id == request.params.id);
+	const booklistTitle = booklist.find(
+		(book) => book.title == request.params.title
+	);
+	const booklistAuthor = booklist.find(
+		(book) => book.author == request.params.author
+	);
+	if (!booklistId && !booklistTitle && !booklistAuthor) {
+		if (request.route.path == `/books/id=:id`) {
+			return next(createError(404, `There is no book with that id`));
+		}
+		if (request.route.path == `/books/title=:title`) {
+			return next(createError(404, `There is no book with that title`));
+		}
+		if (request.route.path == `/books/author=:author`) {
+			return next(createError(404, `There is no book by that author`));
+		}
+		return next(createError(404, `That book does not exist`));
 	}
-	response.json(booklistitem);
+	if (booklistTitle) {
+		response.json(
+			booklist.filter((book) => book.title == request.params.title)
+		);
+	}
+	if (booklistAuthor) {
+		response.json(
+			booklist.filter((book) => book.author == request.params.author)
+		);
+	}
+	response.json(booklist.filter((book) => book.id == request.params.id));
 };
 
 exports.delete = function (request, response, next) {
-	const booklistitem = booklist.find((book) => book.id == request.params.id);
-	if (!booklistitem) {
-		return next(createError(404, `There is no book with that id`));
+	const booklistId = booklist.find((book) => book.id == request.params.id);
+	const booklistTitle = booklist.find(
+		(book) => book.title == request.params.title
+	);
+	const booklistAuthor = booklist.find(
+		(book) => book.author == request.params.author
+	);
+	if (!booklistId && !booklistTitle && !booklistAuthor) {
+		if (request.route.path == `/books/id=:id`) {
+			return next(createError(404, `There is no book with that id`));
+		}
+		if (request.route.path == `/books/title=:title`) {
+			return next(createError(404, `There is no book with that title`));
+		}
+		if (request.route.path == `/books/author=:author`) {
+			return next(createError(404, `There is no book by that author`));
+		}
+		return next(createError(404, `That book does not exist`));
 	}
-	booklist = booklist.filter((book) => book.id != request.params.id);
+	if (booklistTitle) {
+		booklist = booklist.filter(
+			(book) => book.title != request.params.title
+		);
+	} else if (booklistAuthor) {
+		booklist = booklist.filter(
+			(book) => book.author != request.params.author
+		);
+	} else {
+		booklist = booklist.filter((book) => book.id != request.params.id);
+	}
 	response.json({ result: true });
 };
 
 exports.update = function (request, response, next) {
-	const booklistitem = booklist.find((book) => book.id == request.params.id);
+	const booklistId = booklist.find((book) => book.id == request.params.id);
+	const booklistTitle = booklist.find(
+		(book) => book.title == request.params.title
+	);
+	const booklistAuthor = booklist.find(
+		(book) => book.author == request.params.author
+	);
+	if (!booklistId && !booklistTitle && !booklistAuthor) {
+		if (request.route.path == `/books/id=:id`) {
+			return next(createError(404, `There is no book with that id`));
+		}
+		if (request.route.path == `/books/title=:title`) {
+			return next(createError(404, `There is no book with that title`));
+		}
+		if (request.route.path == `/books/author=:author`) {
+			return next(createError(404, `There is no book by that author`));
+		}
+		return next(createError(404, `That book does not exist`));
+	}
 	if (!request.body.title) {
 		return next(createError(400, `Title of book is required`));
 	}
@@ -59,16 +130,36 @@ exports.update = function (request, response, next) {
 			createError(400, `Read status of book must be a boolean value`)
 		);
 	}
-	if (!booklistitem) {
-		return next(createError(404, `There is no book with that id`));
+	if (booklistTitle) {
+		booklist = booklist.map((book) => {
+			console.log(book);
+			if (book.title == request.params.title) {
+				book.title = request.body.title;
+				book.author = request.body.author;
+				book.read = request.body.read;
+			}
+			return book;
+		});
+	} else if (booklistAuthor) {
+		booklist = booklist.map((book) => {
+			console.log(book);
+			if (book.author == request.params.author) {
+				book.title = request.body.title;
+				book.author = request.body.author;
+				book.read = request.body.read;
+			}
+			return book;
+		});
+	} else {
+		booklist = booklist.map((book) => {
+			console.log(book);
+			if (book.id == request.params.id) {
+				book.title = request.body.title;
+				book.author = request.body.author;
+				book.read = request.body.read;
+			}
+			return book;
+		});
 	}
-	booklist = booklist.map((book) => {
-		if (book.id == request.params.id) {
-			(book.title = request.body.title)(
-				(book.author = request.body.author)
-			)((book.read = request.body.read));
-		}
-		return book;
-	});
 	response.json({ result: true });
 };
